46113e0af78b3e425a8bcb033fbb9e4c9ced58b9
Parker Sprouse <parker@parkersprouse.me>
2025-01-12 12:23:16 -0600

    Switched from vanilla audio to howler.js

--- src/components/DropPlayer.vue
+++ src/components/DropPlayer.vue
@@ -4,19 +4,6 @@
     clickable
     @click='onClick'
   >
-    <audio
-      :id='domify(drop.label)'
-      ref='audio_ele'
-      preload='auto'
-      style='display: none !important;'
-    >
-      <source
-        v-for='file in drop.files'
-        :key='domify(file)'
-        :src='file'
-        :type='`audio/${getFileType(file)}`'
-      >
-    </audio>
     <q-item-section avatar>
       <q-icon
         :name='playing ? evaStopCircle : evaPlayCircleOutline'
@@ -34,8 +21,9 @@
 <script setup lang='ts'>
 import { evaPlayCircleOutline, evaStopCircle } from '@quasar/extras/eva-icons';
 import { get, set } from '@vueuse/core';
+import { Howl } from 'howler';
 import { useQuasar } from 'quasar';
-import { inject, onMounted, onUnmounted, ref, useTemplateRef, watch } from 'vue';
+import { inject, onMounted, onUnmounted, ref } from 'vue';
 
 import type { Drop } from 'src/types/drop.js';
 import type { DropEventBus } from 'src/types/drop_event_bus.js';
@@ -56,78 +44,69 @@ const $q = useQuasar();
 
 /*-- Data --*/
 
-const audio_ele = useTemplateRef<HTMLAudioElement>('audio_ele');
+const audio = new Howl({
+  autoplay: false,
+  html5: false,
+  loop: false,
+  mute: false,
+  pool: drop.files.length,
+  preload: true,
+  rate: 1,
+  src: [...drop.files],
+  volume: 1,
+});
 const playing = ref<boolean>(false);
-
-
-/*-- Watch --*/
-
-watch(audio_ele, (new_val) => {
-  console.log(new_val);
-  if (!new_val) set(playing, false);
-  else set(playing, !new_val);
-}, { deep: true, flush: 'post' });
 
 
 /*-- Methods --*/
 
-function domify(text: string): string {
-  return text.toLocaleLowerCase().trim().replace(/\s+/g, '_').replace(/\W+/g, '');
-}
-
-function getFileType(file: string): string | undefined {
-  return file.split('.').at(-1);
-}
-
 function onClick(): void {
-  resetPlayer();
-  if (!get(playing)) {
-    get(audio_ele)!.play()
-      .then(() => {
-        $bus.emit('drop_started', drop.label);
-      })
-      .catch((err) => {
-        resetPlayer();
-        $q.notify({
-          color: 'negative',
-          // message: `Failed to play "${drop.label}" :(`,
-          message: `${drop.label} | ${err}`,
-        });
-      });
+  if (get(playing)) resetAudio();
+  else {
+    audio.play();
+    $bus.emit('drop_started', drop.label);
   }
 }
 
 /* Audio Event Listeners */
 
 function onDropStarted(label: string): void {
-  if (label !== drop.label) resetPlayer();
+  if (label !== drop.label) resetAudio();
 }
 
-function onEnded(): void {
-  resetPlayer();
+function resetAudio(): void {
+  audio.stop();
+  set(playing, false);
 }
 
-function resetPlayer(): void {
-  const player = get(audio_ele)!;
-  if (get(playing)) player.pause();
-  player.currentTime = 0;
+function handleError(id: number, error: unknown): void {
+  resetAudio();
+  $q.notify({
+    color: 'negative',
+    message: `${drop.label} | ${error as Error}`,
+  });
 }
 
 
 /*-- Component Lifecycle --*/
 
 onMounted(() => {
-  get(audio_ele)!.addEventListener('ended', onEnded);
+  audio.on('loaderror', handleError);
+  audio.on('playerror', handleError);
+  audio.on('end', resetAudio);
+  audio.on('play', () => {
+    set(playing, true);
+  });
 
   $bus.on('drop_started', onDropStarted);
-  $bus.on('stop_all_drops', resetPlayer);
+  $bus.on('stop_all_drops', resetAudio);
 });
 
 onUnmounted(() => {
-  get(audio_ele)!.removeEventListener('ended', onEnded);
+  audio.off();
 
   $bus.off('drop_started', onDropStarted);
-  $bus.off('stop_all_drops', resetPlayer);
+  $bus.off('stop_all_drops', resetAudio);
 });
 </script>
 
